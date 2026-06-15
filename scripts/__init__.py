d1380e3e7eac22a1047df5574542371403c1865d
hungryidiot <2647226993@qq.com>
2026-06-15 17:12:24 +0800

    Add scripts package initializer

---
+++ scripts/__init__.py
@@ -0,0 +1,7 @@
+"""Experiment scripts package.
+
+This file intentionally avoids importing script modules so that
+`from scripts.utils import ...` stays lightweight and side-effect free.
+"""
+
+__all__: list[str] = []
